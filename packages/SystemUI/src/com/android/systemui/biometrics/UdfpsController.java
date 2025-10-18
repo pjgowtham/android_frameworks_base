3522d5f9b41da5c0d7b5e896c3e3291a05391056
pjgowtham <pjgowtham@gmail.com>
2025-10-18 19:01:26 +0530

    Revert "UdfpsHelper: Track brightness mirror to hide/show Udfps dim layer"
    This reverts commit 1c21def667917f91bcdb85c1660581e180cfa1fa.

--- packages/SystemUI/src/com/android/systemui/biometrics/UdfpsController.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/UdfpsController.java
@@ -99,7 +99,6 @@ import com.android.systemui.log.SessionTracker;
 import com.android.systemui.plugins.FalsingManager;
 import com.android.systemui.plugins.statusbar.StatusBarStateController;
 import com.android.systemui.power.domain.interactor.PowerInteractor;
-import com.android.systemui.settings.brightness.domain.interactor.BrightnessMirrorShowingInteractor;
 import com.android.systemui.shade.ShadeDisplayAware;
 import com.android.systemui.shade.domain.interactor.ShadeInteractor;
 import com.android.systemui.shared.system.SysUiStatsLog;
@@ -191,7 +190,6 @@ public class UdfpsController implements DozeReceiver, Dumpable {
     @NonNull private final SelectedUserInteractor mSelectedUserInteractor;
     private final boolean mIgnoreRefreshRate;
     private final KeyguardTransitionInteractor mKeyguardTransitionInteractor;
-    @NonNull private final BrightnessMirrorShowingInteractor mBrightnessMirrorShowingInteractor;
 
     // Currently the UdfpsController supports a single UDFPS sensor. If devices have multiple
     // sensors, this, in addition to a lot of the code here, will be updated.
@@ -299,8 +297,7 @@ public class UdfpsController implements DozeReceiver, Dumpable {
                         mShadeInteractor,
                         mUdfpsOverlayInteractor,
                         mPowerInteractor,
-                        mScope,
-                        mBrightnessMirrorShowingInteractor
+                        mScope
                     )));
         }
 
@@ -699,8 +696,7 @@ public class UdfpsController implements DozeReceiver, Dumpable {
             @NonNull UdfpsOverlayInteractor udfpsOverlayInteractor,
             @NonNull PowerInteractor powerInteractor,
             @Application CoroutineScope scope,
-            UserActivityNotifier userActivityNotifier,
-            @NonNull BrightnessMirrorShowingInteractor brightnessMirrorShowingInteractor) {
+            UserActivityNotifier userActivityNotifier) {
         mContext = context;
         mExecution = execution;
         mVibrator = vibrator;
@@ -743,7 +739,6 @@ public class UdfpsController implements DozeReceiver, Dumpable {
         mAlternateBouncerInteractor = alternateBouncerInteractor;
         mUdfpsOverlayInteractor = udfpsOverlayInteractor;
         mPowerInteractor = powerInteractor;
-        mBrightnessMirrorShowingInteractor = brightnessMirrorShowingInteractor;
         mScope = scope;
         mInputManager = inputManager;
         mSelectedUserInteractor = selectedUserInteractor;
--- packages/SystemUI/src/com/android/systemui/biometrics/UdfpsControllerOverlay.kt
+++ packages/SystemUI/src/com/android/systemui/biometrics/UdfpsControllerOverlay.kt
@@ -52,7 +52,6 @@ import com.android.systemui.keyguard.domain.interactor.KeyguardTransitionInterac
 import com.android.systemui.keyguard.shared.model.KeyguardState
 import com.android.systemui.power.domain.interactor.PowerInteractor
 import com.android.systemui.res.R
-import com.android.systemui.settings.brightness.domain.interactor.BrightnessMirrorShowingInteractor
 import com.android.systemui.shade.domain.interactor.ShadeInteractor
 import com.android.systemui.statusbar.policy.KeyguardStateController
 import dagger.Lazy
@@ -90,7 +89,6 @@ constructor(
     private val udfpsOverlayInteractor: UdfpsOverlayInteractor,
     private val powerInteractor: PowerInteractor,
     @Application private val scope: CoroutineScope,
-    private val brightnessMirrorShowingInteractor: BrightnessMirrorShowingInteractor,
 ) {
     private val currentStateUpdatedToOffAodOrDozing: Flow<Unit> =
         transitionInteractor.currentKeyguardState
@@ -121,7 +119,7 @@ constructor(
     )
 
     private val udfpsHelper: UdfpsHelper? = if (useFrameworkDimming) {
-        UdfpsHelper(context, windowManager, shadeInteractor, requestReason, brightnessMirrorShowingInteractor)
+        UdfpsHelper(context, windowManager, shadeInteractor, requestReason)
     } else {
         null
     }
